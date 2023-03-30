fa7036a86c7df8d9cfa3df5e73afd8a616ec9343
Unreal <t.babinos@gmail.com>
2023-03-30 15:05:11 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import { Alchemy, Network } from 'alchemy-sdk';
+import { useEffect, useState } from 'react';
+import { Routes,Route, Link, NavLink } from 'react-router-dom';
+import './App.css';
+
+import OneBlock from './components/OneBlock';
+import Main from './components/Main';
+import './style.css'
+import OneTransaction from './components/OneTransaction';
+import Account from './components/Account';
+import Header from './components/Header';
+
+
+// Refer to the README doc for more information about using API
+// keys in client-side code. You should never do this in production
+// level code.
+const settings = {
+  apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+  network: Network.ETH_MAINNET,
+};
+
+
+// In this week's lessons we used ethers.js. Here we are using the
+// Alchemy SDK is an umbrella library with several different packages.
+//
+// You can read more about the packages here:
+//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
+const alchemy = new Alchemy(settings);
+
+function App() {
+  const [blockNumber, setBlockNumber] = useState("");
+  const [block,setBlock]=useState({})
+  return(
+    <>
+    <Header/>
+    
+    <Routes>
+      <Route path='/' element={<Main blockNumber={blockNumber}
+                               setBlockNumber={setBlockNumber}
+                               alchemy={alchemy}
+                               block={block}/>}/>
+      <Route path='/block/:url' element={<OneBlock  blockNumber={blockNumber}
+                                                    alchemy={alchemy}
+                                                    block={block}
+                                                    setBlock={setBlock}/>} />
+      <Route path='/transactions/:url' element={<OneTransaction
+      alchemy={alchemy}/>}/>
+      <Route path='/accounts/:url' element={<Account
+      alchemy={alchemy}/>}/>
+      
+    </Routes>
+    </>
+  )
+}
+
+export default App;
+/* <div className="App">Latest Block Number: {blockNumber}
+   <OneBlock
+  blockNumber={blockNumber}
+  alchemy={alchemy}/> 
+  <MemoizedBlock
+  alchemy={alchemy}
+  blockNumber={blockNumber}
+  />
+  </div>;*/ 
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Routes,Route,Link,NavLink } from 'react-router-dom';
+
+
+export default function Header() {
+  return (
+    <div className='App-header'>
+      <nav>
+        
+      </nav>
+    </div>
+  );
+}
